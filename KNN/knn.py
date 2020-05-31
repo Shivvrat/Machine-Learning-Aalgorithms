fdd37785a4a111d0b6fa8ba1eb97eb409baeac25
Shivvrat Arya <Shivvrat@users.noreply.github.com>
2020-05-31 06:42:08 -0500

    update knn

--- KNN/knn.py
+++ KNN/knn.py
@@ -9,20 +9,26 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
+
 def train(examples, class_of_examples):
+	"""
+	This function just creates the training data
+	:param examples: The examples from given dataset
+	:param class_of_examples: The class of the given examples
+	:return: A matrix containing both the examples and its respective classes
+	"""
 	return numpy.hstack((examples, class_of_examples))
 
 
 def test_classification(train_data, test_point, index_of_prediction_value = -1, k_value = 3):
-	distance = []
-	for each_point in train_data:
-		distance.append(numpy.linalg.norm(each_point[:-1] - test_point))
-	train_points, distance = zip(*sorted(zip(train_data, distance)))
-	classes = train_points[:k_value, index_of_prediction_value]
-	return numpy.median(classes)
-
-
-def test_regression(train_data, test_point, index_of_prediction_value = -1, k_value = 3):
+	"""
+	This function is used to predict the class for the given test point
+	:param train_data: The training data set
+	:param test_point: The point for which we want to find the class
+	:param index_of_prediction_value: The index of the class variable in the dataset
+	:param k_value: The value of k
+	:return: The class of the test example
+	"""
 	distance = []
 	for each_point in train_data:
 		distance.append(numpy.linalg.norm(each_point[:-1] - test_point))
@@ -30,19 +36,58 @@ def test_regression(train_data, test_point, index_of_prediction_value = -1, k_va
 	zipped_pairs = zip(distance, train_data)
 	train_points = [x for _, x in sorted(zipped_pairs)]
 	train_points = numpy.array(train_points)
-	color_score = train_points[:k_value, index_of_prediction_value ]
-	return numpy.mean(color_score)
+	output_val = train_points[:k_value, index_of_prediction_value]
+	return numpy.mode(output_val)
+
+
+def test_regression(train_data, test_point, index_of_prediction_value = -1, k_value = 3):
+	"""
+	This function is used to predict the value of output for the given test point
+	:param train_data: The training data set
+	:param test_point: The point for which we want to find the value of output
+	:param index_of_prediction_value: The index of the output variable in the dataset
+	:param k_value: The value of k
+	:return: The value of output for the test example
+	"""
+	distance = []
+	for each_point in train_data:
+		distance.append(numpy.linalg.norm(each_point[:-1] - test_point))
+	train_data = numpy.array(train_data).tolist()
+	zipped_pairs = zip(distance, train_data)
+	train_points = [x for _, x in sorted(zipped_pairs)]
+	train_points = numpy.array(train_points)
+	output_val = train_points[:k_value, index_of_prediction_value]
+	return numpy.mean(output_val)
 
 
 def evaluation_classification(true_class, predicted_class):
+	"""
+	This function is used to evaluate the given model
+	:param true_class: The true class vector
+	:param predicted_class: The classes predicted for the same testing examples
+	:return: The accuracy
+	"""
 	return accuracy_score(true_class, predicted_class)
 
 
 def evaluation_regression(true_val, predicted_val):
+	"""
+	This function is used to evaluate the given model
+	:param true_class: The true class vector
+	:param predicted_class: The classes predicted for the same testing examples
+	:return: The mean_squared_error
+	"""
 	return mean_squared_error(true_val, predicted_val)
 
 
 def knn_classification(train_dataset, test_dataset, k_value):
+	"""
+	This is the main function to do classification by knn
+	:param train_dataset: This contains the examples on which we will train our model
+	:param test_dataset: THis contains the examples on which we will test our model
+	:param k_value: The value of k in knn
+	:return: The accuracy score
+	"""
 	predicted_class = []
 	for each_test_example in test_dataset:
 		class_of_test_example = test_classification(train_dataset, each_test_example[:-1], k_value)
@@ -52,6 +97,13 @@ def knn_classification(train_dataset, test_dataset, k_value):
 
 
 def knn_regression(train_dataset, test_dataset, k_value):
+	"""
+	This is the main function to do regression by knn
+	:param train_dataset: This contains the examples on which we will train our model
+	:param test_dataset: THis contains the examples on which we will test our model
+	:param k_value: The value of k in knn
+	:return: The mean squared error
+	"""
 	predicted_val = []
 	for each_test_example in test_dataset:
 		val_of_test_example = test_regression(train_dataset, each_test_example[:-1], -1, k_value)
@@ -61,12 +113,22 @@ def knn_regression(train_dataset, test_dataset, k_value):
 
 
 def read_data(directory):
+	"""
+	This function is used to read data from the given directory
+	:param directory: The directory in which the data is stored
+	:return: The data as a pandas dataframe
+	"""
 	from numpy import genfromtxt
 	my_data = pandas.read_table(directory, header = "infer")
 	return my_data
 
 
 def dataset_split(data):
+	"""
+	Here we split the dataset into train and test parts
+	:param data: The given whole dataset
+	:return: Train data and test data as numpy arrays
+	"""
 	train_data, test_data = train_test_split(data, test_size = 0.2)
 	return train_data, test_data
 
